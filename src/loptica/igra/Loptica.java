8dcb07337105e6f68d1b94942c0a910825f5675b
M <mc.cm.mail@gmail.com>
2018-07-06 23:51:56 +0200

    [NFC] adding licensing information

--- src/loptica/igra/Loptica.java
+++ src/loptica/igra/Loptica.java
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 package loptica.igra;
 
 import java.util.ArrayList;
--- src/loptica/igra/PravougaonaPrepreka.java
+++ src/loptica/igra/PravougaonaPrepreka.java
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 package loptica.igra;
 
 import javafx.scene.paint.Color;
